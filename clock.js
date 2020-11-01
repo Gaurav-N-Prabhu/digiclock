e88fc320e98ef1b1386ff449363b40b6243f0c0d
prabhu-gaurav <54278211+prabhu-gaurav@users.noreply.github.com>
2020-11-01 20:53:45 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -4,8 +4,6 @@ function realtime()
     var m = document.getElementById('mint');
     var s = document.getElementById('sec');
 
-
-
     var h = new Date().getHours();
     var m = new Date().getMinutes();
     var s = new Date().getSeconds();
@@ -13,10 +11,19 @@ function realtime()
     h = (h < 10) ? "0" + h : h
     m = (m < 10) ? "0" + m : m
     s = (s < 10) ? "0" + s : s
+	
+	if(h>12)
+	{
 
-    hr.innerHTML = h;
-    mint.innerHTML = m;
-    sec.innerHTML = s;
+		hr.innerHTML = h-12;
+		mint.innerHTML = m;
+		sec.innerHTML = s;
+	}
+	else
+	{
+		hr.innerHTML = h;
+		mint.innerHTML = m;
+		sec.innerHTML = s;
+	}
 }
-
 var interval = setInterval(realtime, 0);
